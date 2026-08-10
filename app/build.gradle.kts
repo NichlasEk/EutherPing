e22081d27c784bb9538dc81ab0ec422a379bb2df
Nichlas <nichlas.eklof@gmail.com>
2026-08-10 21:30:51 +0200

    Release EutherPing 0.8.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "se.apothictech.eutherping"
         minSdk = 28
         targetSdk = 36
-        versionCode = 46
-        versionName = "0.8.18"
+        versionCode = 47
+        versionName = "0.8.19"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
